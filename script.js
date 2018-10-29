ac97033bc019c8bdbaeb97bd1521a1bb48b72053
sowluss <sowluss@gmail.com>
2018-10-30 01:17:16 +0200

    added random sounds to each click using Math.floor, Math.random

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ $('section').click(function() {
   let colorDiv = $("<div class='color'></div>");
   $('section').append(colorDiv);
 })
+
+ let sounds = [
+   new Audio("sounds/Cellar-door.mp3"),
+   new Audio("sounds/Chord.mp3"),
+   new Audio("sounds/Female-Scream.mp3"),
+   new Audio("sounds/Laugh.mp3"),
+   new Audio("sounds/Female.mp3"),
+   new Audio("sounds/Loud_Bang.mp3"),
+   new Audio("sounds/Psycho.mp3"),
+   new Audio("sounds/Scream.mp3"),
+   new Audio("sounds/tolling-bell.mp3")
+ ];
+
+ let play = document.getElementById('playSound');
+ play.addEventListener("click", function() {
+ 
+   let random = sounds[Math.floor(Math.random() * sounds.length)];
+   random.play();
+ 
+ })
+ 
